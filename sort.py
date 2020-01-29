d4f06d22c40dff61b4ffc5150dad34300099823d
amznero <398994055@qq.com>
2020-01-30 01:29:26 +0800

    add bubble/selection/insertion sorting code

--- sort.py
+++ sort.py
@@ -1,13 +1,34 @@
 def bubble_sort(data):
-    raise NotImplementedError
+    data_len = data.shape[0]
+    for i in range(data_len - 1):
+        for j in range(data_len - i - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+            yield data
 
 
 def selection_sort(data):
-    raise NotImplementedError
+    data_len = data.shape[0]
+    for i in range(data_len - 1):
+        min_idx = i
+        for j in range(i + 1, data_len):
+            if data[j] < data[min_idx]:
+                min_idx = j
+        data[i], data[min_idx] = data[min_idx], data[i]
+        yield data
 
 
 def insertion_sort(data):
-    raise NotImplementedError
+    data_len = data.shape[0]
+    for i in range(1, data_len):
+        anchor = data[i]
+        j = i - 1
+        while anchor < data[j] and j >= 0:
+            data[j + 1] = data[j]
+            j -= 1
+            yield data
+        data[j + 1] = anchor
+        yield data
 
 
 def shell_sort(data):
@@ -23,6 +44,10 @@ def quick_sort(data):
 
 
 if __name__ == "__main__":
-    from utils import generate_data
-    data = generate_data()
-    quick_sort(data)
+    import numpy as np
+
+    data = np.array([0, 1, 2, -1, -5, -3, 5, 2, 7])
+    res = bubble_sort(data)
+    print(data)
+    for each in res:
+        print(each)
